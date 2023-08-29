860b973c4a2c005ac73ea5b1c15b56f51208db63
ngedeone <ochiengwycliff122@gmail.com>
2023-08-29 20:34:05 +0300

    task 1

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
 """A class Square is defined."""
+
+
 class Square:
     """here is the square."""
+
     pass
---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""a class Square is defined."""
+
+class Square:
+    """Represent the class."""
+
+    def __init__(self, size):
+        """a new Square is initialized.
+
+        Args:
+            size (int): The size of the new square.
+        """
+        self.__size = size
