9f9c2ddf74108f4ebfab14b6c078a14e9d3f0603
ngedeone <ochiengwycliff122@gmail.com>
2023-09-12 05:18:25 +0300

    inheritance

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,17 +1,7 @@
 #!/usr/bin/python3
+"""Defines an object attribute lookup function."""
 
 
-class MyClass1(object):
-    pass
-
-
-class MyClass2(object):
-    my_attr1 = 3
-
-    def my_meth(self):
-        pass
-
-
-print(lookup(MyClass1))
-print(lookup(MyClass2))
-print(lookup(int))
+def lookup(obj):
+    """Return a list of an object's available attributes."""
+    return (dir(obj))
